04e6536fb91e3c7a0d176b7cf3fb8c9cd346f3e1
windyfrost <81542547+windyfrost@users.noreply.github.com>
2024-01-10 12:07:34 +0800

    Update bypass.js
    Improved coding.

--- bypass.js
+++ bypass.js
@@ -1,18 +1,17 @@
 javascript:(function() {
-  let hardOverlay = document.getElementsByClassName('hardsellOverlay')[0];
+  const hardOverlay = document.querySelector('.hardsellOverlay');
   if (hardOverlay) {
     hardOverlay.remove();
   }
-  document.getElementsByTagName("body")[0].style.overflow = "scroll";
-  document.getElementsByTagName('body')[0].style.position = 'unset'
-  let style = document.createElement('style');
-  style.innerHTML = `
-    #LoginModal {
-      display: none!important;
-    }
-  `;
+
+  document.body.style.overflow = 'scroll';
+  document.body.style.position = 'unset';
+
+  const style = document.createElement('style');
+  style.textContent = '#LoginModal { display: none !important; }';
   document.head.appendChild(style);
-  window.addEventListener("scroll", function (event) {
+
+  window.addEventListener('scroll', function(event) {
     event.stopPropagation();
   }, true);
 })();
